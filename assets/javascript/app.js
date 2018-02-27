da4e459eb8a37a7c083661f21a2db07b1cb9c915
Joonho Lee <joonholee@Joonhos-Air.home>
2018-02-26 21:35:12 -0500

    edited app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,36 +26,62 @@ var time;
       clearInterval(intervalId);
     }
 // ------------------------------------------------------------------------
-var questions = ["Which two actors directed themselves in movie and won Oscars for Best Actor?",
-"“After all, tomorrow is another day!” Was the last line in which Oscar-winning Best Picture?",
-"Who is the only person to win an Oscar for Best Director for the only movie he ever directed?",
-"Who is the most nominated actor in Academy history?",
-"In which movie was the Oscar-winning song “Do Not Forsake Me, Oh My Darlin” prominently featured?",
-"Which classic Billy Wilder film noir was the basis for the plot in director Lawrence Kasdan’s neo-noir Body Heat, starring Kathleen Turner and William Hurt?",
-"What was the first Western to win the Academy Award for Best Picture?", "What was the first film to receive an Academy Award nomination in each of the performance categories?",
-"What Cameron Crowe-directed Hollywood remake was the setting for the on-screen and real-life romance of married actor Tom Cruise and Spanish star Penelope Cruz?",
-"Which movie ends with this final line of dialogue: “Why, she wouldn’t even harm a fly”?"]
-var mutipleChoice ={
-Q1:["A. Al Pacino and Timothy Hutton" , "B. Jack Nicholson and Kevin Spacey", "C. Laurence Oliver and Roberto Benigni", "D. Tom Hanks and Paul Newman"],
-Q2:["A. Gone With the Wind", "B. Great Expectations", "C. Harold and Maude", "D. The Matrix "],
-Q3:["A. Bob Fosse", "B. Frank Borzage", "C. Leo McCarey", "D. Jerome Robbins"],
-Q4:["A. Jack Nicholson", "B. Laurence Olivier", "C. Spencer Tracy", "D. Paul Newman"],
-Q5:["A. Butch Cassidy and the Sundance Kid", "B. Calamity Jane", "C. High Noon", "D.Song of the South"],
-Q6:["A. Ace in the Hole", "B. Double Indemnity", "C. The Maltese Falcon", "D. Sunset Boulevard"],
-Q7:["A. Brokeback Mountain", "B. Cimarron", "C. The Last Wagon", "D. Stagecoach"],
-Q8:["A. A Streetcar Named Desire", "B. The Color Purple", "C. My Man Godfrey", "D. The Turning Point"],
-Q9:["A.Magnolia", "B. Masked and Anonymous", "C. Vanila Sky", "D. Waking up in Reno"],
-Q10:["A. Diabolique", "B. Psycho", "C. To die For", "D. Monster"]
-}
+var Q1 = [{question:"Which two actors directed themselves in movie and won Oscars for Best Actor?",
+answers:["Al Pacino and Timothy Hutton" , "Jack Nicholson and Kevin Spacey", "Laurence Oliver and Roberto Benigni", "Tom Hanks and Paul Newman"],
+rightAnswer:"Laurence Oliver and Roberto Benigni" }]
 
-function questionGenerator(){
-for(var i = 0 ; i< questions.length ; i++){
- $("#body").append("<h3> " + (i+1) + ". " + questions[i] + "</h3>");
- }
- for(var j = 0; j<mutipleChoice.length; j++){
-$("#body").append("<h3>"  + mutipleChoice[j] + "</h3>" )
- console.log(mutipleChoice[j]);
- }
+var Q2 = [{question:"“After all, tomorrow is another day!” Was the last line in which Oscar-winning Best Picture?",
+answers:["Gone With the Wind", "Great Expectations", "Harold and Maude", "The Matrix "],
+rightAnswer:"Gone With the Wind" 
+}]
+
+var Q3 = [{question: "Who is the only person to win an Oscar for Best Director for the only movie he ever directed?", 
+answers:["Bob Fosse", "Frank Borzage", "Leo McCarey", "Jerome Robbins"],
+rightAnswer: "Jerome Robbins" 
+}]
+
+var Q4 = [{question:"Who is the most nominated actor in Academy history?",
+answers:["Jack Nicholson", "Laurence Olivier", "Spencer Tracy", "Paul Newman"],
+rightAnswer: "Jack Nicholson"
+}]
+
+var Q5 = [{question:"In which movie was the Oscar-winning song 'Do Not Forsake Me, Oh My Darlin' prominently featured?",
+answers:["Butch Cassidy and the Sundance Kid", "Calamity Jane", "High Noon", "Song of the South"],
+rightAnswer:"High Noon"
+}]
+
+var Q6 = [{question:"Which classic Billy Wilder film noir was the basis for the plot in director Lawrence Kasdan’s neo-noir Body Heat, starring Kathleen Turner and William Hurt?",
+answers:["Ace in the Hole", "Double Indemnity", "The Maltese Falcon", "Sunset Boulevard"],
+rightAnswer:"Double Idemnity"
+}]
+
+var Q7 = [{question:"What was the first Western to win the Academy Award for Best Picture?",
+answers:["Brokeback Mountain", "Cimarron", "The Last Wagon", "Stagecoach"],
+rightAnswer:"Cimarron"
+}]
+
+var Q8= [{question:"What was the first film to receive an Academy Award nomination in each of the performance categories?",
+answers:["A Streetcar Named Desire", "The Color Purple", "My Man Godfrey", "The Turning Point"],
+rightAnswer:"My Man Godfrey"
+}]
+
+var Q9 = [{question:"What Cameron Crowe-directed Hollywood remake was the setting for the on-screen and real-life romance of married actor Tom Cruise and Spanish star Penelope Cruz?",
+answers:["Magnolia", "Masked and Anonymous", "Vanila Sky", "Waking up in Reno"],
+rightAnswer:"Vanila Sky"
+}]
+
+var Q10= [{quetion:"Which movie ends with this final line of dialogue: “Why, she wouldn’t even harm a fly?",
+answers:["Diabolique", "Psycho", "To die For", "Monster"],
+rightAnswer:"Psycho"
+}]
+
+console.log(game[1]);
+var questionSection = $("<#questionSection>")
+$("#body").append(questionSection)
+
+
+function questionGenerator(arr){
+
  
 }
 
